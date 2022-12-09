fa2f172292a0536c95350d4e96c8c47fa94e7823
Dominic Klahn <120129100+DKlahnF22@users.noreply.github.com>
2022-12-09 17:44:20 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,221 @@
+const gameState = {};
+gameState.player;
+var bullets;
+var fire;
+var cursors;
+var text;
+var MSpeed = 1;
+var XP = 0;
+var lastFired = 0;
+var PBDelay = 1500;
+var diff = 0;
+function preload(){
+  this.load.image('spaceship', 'assets/images/spaceship.jpg');
+  this.load.image('shoe', 'assets/images/shoe.png');
+  this.load.image('PBullet', 'assets/images/PBullet.png');
+  this.load.image('enemy', 'assets/images/contact-photo.jpg');
+}
+
+function create(){
+  //start of enemy spawning
+  var Enemy1 = new Phaser.Class({
+      Extends: Phaser.Physics.Arcade.Image,
+      initialize:
+      function Spawn (){
+        num = 4 + diff;
+        for(let i=0;i<num;i++){
+          
+        }
+      }
+  });
+  //end of enemy spawning
+  //start of bullet stuff
+  var Bullet = new Phaser.Class({
+
+        Extends: Phaser.Physics.Arcade.Image,
+
+        initialize:
+
+        function Bullet (scene)
+        {
+            Phaser.Physics.Arcade.Image.call(this, scene, 0, 0, 'PBullet');
+          this.setScale(0.25);
+
+            this.setBlendMode(1);
+            this.setDepth(1);
+
+            this.speed = 1000;
+            this.lifespan = 1000;
+
+            this._temp = new Phaser.Math.Vector2();
+        },
+
+        fire: function (ship)
+        {
+            this.lifespan = 1000;
+
+            this.setActive(true);
+            this.setVisible(true);
+            // this.setRotation(ship.rotation);
+            this.setAngle(gameState.player.body.rotation);
+            this.setPosition(gameState.player.x, gameState.player.y);
+            this.body.reset(gameState.player.x, gameState.player.y);
+
+            // ship.body.advancePosition(10, this._temp);
+
+            // this.setPosition(this._temp.x, this._temp.y);
+            // this.body.reset(this._temp.x, this._temp.y);
+
+            //  if ship is rotating we need to add it here
+            // var a = ship.body.angularVelocity;
+
+            // if (ship.body.speed !== 0)
+            // {
+            //     var angle = Math.atan2(ship.body.velocity.y, ship.body.velocity.x);
+            // }
+            // else
+            // {
+                var angle = Phaser.Math.DegToRad(gameState.player.body.rotation);
+            // }
+
+            // this.body.world.velocityFromRotation(angle, this.speed + ship.body.speed, this.body.velocity);
+            this.scene.physics.velocityFromRotation(angle, this.speed, this.body.velocity);
+
+            this.body.velocity.x *= 2;
+            this.body.velocity.y *= 2;
+        },
+  
+
+        update: function (time, delta)
+        {
+            this.lifespan -= delta;
+
+            if (this.lifespan <= 0)
+            {
+                this.setActive(false);
+                this.setVisible(false);
+                this.body.stop();
+            }
+        }
+    });
+  // end of bullet stuff
+    gameState.player = this.physics.add.image(400, 300, 'spaceship');
+
+    gameState.player.setDamping(true);
+    gameState.player.setDrag(.97);
+    gameState.player.setMaxVelocity(200 * MSpeed);
+  // player stuff
+  //start of enemy stuff
+  gameState.enemy1 = this.physics.add.image(800,500, 'enemy');
+  gameState.enemy1.setDamping(false);
+  gameState.enemy1.setDrag(0);
+  gameState.enemy1.setMaxVelocity(400);
+  gameState.enemy1.setScale(.15);
+  gameState.enemy1.setAngle(0);
+
+    gameState.cursors = this.input.keyboard.createCursorKeys();
+    fire = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+    text = this.add.text(10, 10, '', { font: '16px Courier', fill: '#00ff00' });
+    text1 = this.add.text(10, 25, '', { font: '16px Courier', fill: '#00ff00' });
+    text2 = this.add.text(10, 40, '', { font: '16px Courier', fill: '#00ff00' });
+    text3 = this.add.text(10, 55, '', { font: '16px Courier', fill: '#00ff00' });
+    text4 = this.add.text(10, 70, '', { font: '16px Courier', fill: '#00ff00' });
+    text5 = this.add.text(10, 85, '', { font: '16px Courier', fill: '#00ff00' });
+  gameState.bullets = this.physics.add.group({
+        classType: Bullet,
+        maxSize: 30,
+        runChildUpdate: true
+    });
+}
+
+function update(time){
+    // start of movement stuff
+  if (gameState.cursors.up.isDown)
+    {
+        this.physics.velocityFromRotation(gameState.player.rotation, 200, gameState.player.body.acceleration);
+    }
+    else if(gameState.cursors.down.isDown){
+      this.physics.velocityFromRotation(gameState.player.rotation, -50, gameState.player.body.acceleration);
+    }
+    else
+    {
+        gameState.player.setAcceleration(0);
+    }
+    if (gameState.cursors.left.isDown)
+    {
+        gameState.player.setAngularVelocity(-300);
+    }else if (gameState.cursors.right.isDown){
+      gameState.player.setAngularVelocity(300);
+    }else{
+      gameState.player.setAngularVelocity(0);
+    }
+  // fix with if else statment because code is dumb 
+    
+    //end of movement stuff
+  //start of enemy detection
+    // if player to left of enemy AND enemy moving to right (or not moving)
+    
+  //end of enemy detection
+  //start of enemy rotation
+		//const rotation = Phaser.Math.Angle.Between(x, y, tx, ty)
+  //gameState.vec = new Phaser.Math.Vector2(gameState.player.x - gameState.enemy1.x, gameState.player.y - gameState.enemy1.y)
+  gameState.rotation = Phaser.Math.Angle.Between(gameState.enemy1.x, gameState.enemy1.y, gameState.player.x, gameState.player.y);
+		gameState.enemy1.setAngle(Phaser.Math.RadToDeg(gameState.rotation));
+  //end of enemy rotation
+    //start of XP stuff
+    //if(XP == ){
+      
+      
+    //}
+    //end of XP stuff
+    //start of damage stuff (taking damage, dealing damage, damage modifiers)
+    if (gameState.cursors.space.isDown){
+      PBullet(time);
+    }
+  //FINISH THIS (GAME WILL NOT WORK OTHERWISE, FINISH XP TOO)
+
+    text.setText('Speed: ' + gameState.player.body.speed);
+    text1.setText('Rotation: ' + gameState.rotation);
+  text2.setText('Player X:' + gameState.player.x);
+  text3.setText('Player Y:' + gameState.player.y);
+  text4.setText('Enemy X:' + gameState.enemy1.x);
+  text5.setText('Enemy Y:' + gameState.enemy1.y);
+  
+  this.physics.world.wrap(gameState.player, 5);
+
+}
+
+const config = {
+    type: Phaser.AUTO,
+    width: 1350,
+    height: 570,
+    backgroundColor: "b9eaff",
+    physics: {
+        default: 'arcade',
+        arcade: {
+            fps: 60,
+            gravity: {y: 0},
+        }
+    },
+    scene: {
+        preload,
+        create,
+        update
+    }
+};
+
+const game = new Phaser.Game(config);
+//start of functions
+function PBullet(time){
+  if (fire.isDown && time > lastFired)
+    {
+        var bullet = gameState.bullets.get();
+
+        if (bullet)
+        {
+            bullet.fire(gameState.player);
+
+            lastFired = time + 100;
+        }
+    }
+}
